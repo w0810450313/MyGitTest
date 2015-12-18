6363efc4a97b4363147da34fd33f1f309ffd4b92
wang <wang@163.com>
2015-12-18 16:53:02 +0800

    新建项目

---
+++ Mytest/Mytest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mytest
+//
+//  Created by sks on 15/12/18.
+//  Copyright © 2015年 org.lanqiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mytest/Mytest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mytest
+//
+//  Created by sks on 15/12/18.
+//  Copyright © 2015年 org.lanqiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
